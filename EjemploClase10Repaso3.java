7bb0a41a9c70060a5b9a98c31f08ea1f09cb4159
Diego Aguero <dieguito.aguero08@gmail.com>
2023-06-05 11:57:46 -0300

    Agregado nuevos archivos

---
+++ EjemploClase10Repaso3.java
@@ -0,0 +1,11 @@
+public class EjemploClase10Repaso3 {
+    public static void main(String[] args){
+        boolean flag;
+        flag = false;
+        if(flag){
+            System.out.println("Flag es verdadero");
+        }else{
+            System.out.println("Flag es falso");
+        }
+    }
+}
---
+++ EjemploClase10Repaso5.java
@@ -0,0 +1,7 @@
+public class EjemploClase10Repaso5 {
+    public static void main(String[] args){
+        /*pedirle al usuario 3 valores correspondientes a los lados 
+        del triangulo el programa debe mostrar un msj indicando que tipo de triangulo es
+         */
+    }
+}
