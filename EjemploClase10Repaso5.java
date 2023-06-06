1af62cee228a17abd6b24e5f3ad3c76fc74920ca
Diego Aguero <dieguito.aguero08@gmail.com>
2023-06-06 11:00:27 -0300

    Cambios al int por double

--- EjemploClase10Repaso5.java
+++ EjemploClase10Repaso5.java
@@ -1,7 +1,25 @@
 public class EjemploClase10Repaso5 {
     public static void main(String[] args){
-        /*pedirle al usuario 3 valores correspondientes a los lados 
-        del triangulo el programa debe mostrar un msj indicando que tipo de triangulo es
-         */
+/*pedirle al usuario 3 valores correspondientes a los lados 
+ de un triangulo, el programa debe mostrar un mensaje indicando que tipo de triangulo
+ es: equilatero (3 lados iguales) isosceles (2 lados iguales)
+ escaleno (todos sus lados desiguales)
+*/
+        double lado1;
+        double lado2;
+        double lado3;
+
+        System.out.println("Ingrese los 3 lados del triangulo");
+        lado1 = Integer.parseInt(System.console().readLine());
+        lado2 = Integer.parseInt(System.console().readLine());
+        lado3 = Integer.parseInt(System.console().readLine());
+        if((lado1 == lado2) & (lado2 == lado3) & (lado1 == lado3)){
+            System.out.println("Sus 3 lados son iguales, esto es un triangulo equilatero");
+        }else if ((lado1 == lado2) || (lado2 == lado3) || (lado1 == lado3)) {
+            System.out.println("Tiene 2 lados iguales, esto es un triangulo Isosceles");
+
+        }else{
+            System.out.println("Sus lados son todos desiguales, esto es un triangulo escaleno");
+        }
     }
 }
