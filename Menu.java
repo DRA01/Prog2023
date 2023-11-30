96a6b398c101dfb9789cb8d5735be686ddecbb60
Matecitox <129069605+DRA01@users.noreply.github.com>
2023-11-30 11:41:19 -0300

    menu del examen de programacion

---
+++ Menu.java
@@ -0,0 +1,11 @@
+package funciones;
+
+public class Menu {
+    public static void MenuPrincipal(){
+        System.out.println("***1: Muestra el promedio de los numeros cargados***");
+        System.out.println("***2: Muestra la suma de todos los numeros cargados***");
+        System.out.println("***3: Ordena los numeros cargados de manera ascendente***");
+        System.out.println("***4: Ordena los numeros cargados de manera descendente***");
+        System.out.println("***5: Salir***");
+    }
+}
